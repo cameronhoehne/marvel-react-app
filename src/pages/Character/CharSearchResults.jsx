07bdbbae987365a89d3c8d6b13de7d7a59e2664a
Cameron Hoehne <98066436+cameronhoehne@users.noreply.github.com>
2023-04-13 13:12:41 -0700

    Update CharSearchResults.jsx

--- src/pages/Character/CharSearchResults.jsx
+++ src/pages/Character/CharSearchResults.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import CharacSearch from '../../components/Character/CharacSearch';
 import { useParams } from 'react-router-dom';
 import { useNavigate, NavLink } from 'react-router-dom';
-import image from "../../pages/channels4_profile.jpg";
 import { motion as m } from "framer-motion";
 
 function CharSearchResults() {
