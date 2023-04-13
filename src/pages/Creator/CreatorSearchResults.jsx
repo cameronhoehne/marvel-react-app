90ac246e93d04366f07b4da7e49f1a2092c332ae
Cameron Hoehne <98066436+cameronhoehne@users.noreply.github.com>
2023-04-13 13:14:49 -0700

    Update CreatorSearchResults.jsx

--- src/pages/Creator/CreatorSearchResults.jsx
+++ src/pages/Creator/CreatorSearchResults.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import CreatorSearch from '../../components/Creator/CreatorSearch';
 import { useParams } from 'react-router-dom';
 import { useNavigate, NavLink } from 'react-router-dom';
+import image from "../../pages/channels4_profile.webp";
 import { motion as m } from "framer-motion";
 
 function CreatorSearchResults() {
