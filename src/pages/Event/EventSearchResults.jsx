7f5640c84038edcddfe5340b64710e3730aab746
Cameron Hoehne <98066436+cameronhoehne@users.noreply.github.com>
2023-04-13 13:14:38 -0700

    Update EventSearchResults.jsx

--- src/pages/Event/EventSearchResults.jsx
+++ src/pages/Event/EventSearchResults.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import EventSearch from '../../components/Event/EventSearch';
 import { useParams } from 'react-router-dom';
 import { useNavigate, NavLink } from 'react-router-dom';
+import image from "../../pages/channels4_profile.webp";
 import { motion as m } from "framer-motion";
 
 function EventSearchResults() {
